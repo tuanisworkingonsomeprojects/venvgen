75c3578ae55b6ce39a43a345455ff2ca35f993c9
tuanisworkingonsomeprojects <127831837+tuanisworkingonsomeprojects@users.noreply.github.com>
2024-06-29 16:54:14 +0700

    fix bug

--- utils/window_system_control.py
+++ utils/window_system_control.py
@@ -5,7 +5,7 @@ def clear_screen():
     return_status = os.system(clear_screen_command)
 
 def create_venv(project_name):
-    os.system(CREATE_VENV + 'project_name')
+    os.system(CREATE_VENV + project_name)
 
 def install_libraries(venv, libraries):
     os.system(venv + '\\Scripts\\activate.bat\npip install ' + libraries)
