2da9f856e8437ed53e79fe8831ef855e18f6a6de
tuanisworkingonsomeprojects <127831837+tuanisworkingonsomeprojects@users.noreply.github.com>
2024-07-17 15:44:37 +0700

    update the wrong os import

--- venvgen/utils/check_os.py
+++ venvgen/utils/check_os.py
@@ -6,5 +6,5 @@ def check_os() -> ModuleType:
         from ..OS_control import macos_system_control as OS
         return OS
     elif platform.system() == 'win32' or platform.system() == 'Windows':
-        from ..OS_control import macos_system_control as OS
+        from ..OS_control import window_system_control as OS
         return OS
