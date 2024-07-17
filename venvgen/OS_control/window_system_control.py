20a7963b367c35ee4a0dd6f9615541e1b0b08e41
tuanisworkingonsomeprojects <127831837+tuanisworkingonsomeprojects@users.noreply.github.com>
2024-07-17 15:53:47 +0700

    update window system control

--- venvgen/OS_control/window_system_control.py
+++ venvgen/OS_control/window_system_control.py
@@ -11,11 +11,19 @@ def clear_screen():
     return_status = os.system(clear_screen_command)
 
 def create_venv(project_directory, project_name):
-    subprocess.run(['cd', project_directory, 'python', '-m', 'venv', project_name], shell = True)
+    subprocess.run([
+        'cd', project_directory, '&&', 
+        'python', '-m', 'venv', project_name
+        ], shell = True)
     
 
 def install_libraries(project_directory, venv, libraries):
-    os.system(f'cd "{project_directory}"\n' + venv + '\\Scripts\\activate.bat\npip install ' + libraries)
+    # os.system(f'cd "{project_directory}"\n' + venv + '\\Scripts\\activate.bat\npip install ' + libraries)
+    subprocess.run([
+        'cd', project_directory, '&&', 
+        f'{venv}\\Scripts\\activate.bat', '&&', 
+        'pip', 'install'
+        ] + libraries, shell = True)
     
 def install_libraries_with_requirement(project_directory, venv, requirement_file):
     os.system(f'cd "{project_directory}"\n' + venv + '\\Scripts\\activate.bat\npip install -r ' + f'"{requirement_file}"')
