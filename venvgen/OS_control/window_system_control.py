27aa6ea04c494e4a21dae5bcd488b990c99e4232
tuanisworkingonsomeprojects <127831837+tuanisworkingonsomeprojects@users.noreply.github.com>
2024-07-18 02:04:55 +0700

    Update window_system_control.py

--- venvgen/OS_control/window_system_control.py
+++ venvgen/OS_control/window_system_control.py
@@ -11,11 +11,14 @@ def clear_screen():
     return_status = os.system(clear_screen_command)
 
 def create_venv(project_directory, project_name):
+    # subprocess.run([
+    #     'cd', project_directory, '&&', 
+    #     'python', '-m', 'venv', project_name
+    #     ], shell = True)
     subprocess.run([
-        'cd', project_directory, '&&', 
-        'python', '-m', 'venv', project_name
-        ], shell = True)
-    
+    'python', '-m', 'venv', f'{project_directory}\\{project_name}'
+    ], shell = True)
+
 
 def install_libraries(project_directory, venv, libraries):
     # os.system(f'cd "{project_directory}"\n' + venv + '\\Scripts\\activate.bat\npip install ' + libraries)
@@ -26,7 +29,12 @@ def install_libraries(project_directory, venv, libraries):
         ] + libraries, shell = True)
     
 def install_libraries_with_requirement(project_directory, venv, requirement_file):
-    os.system(f'cd "{project_directory}"\n' + venv + '\\Scripts\\activate.bat\npip install -r ' + f'"{requirement_file}"')
+    subprocess.run([
+        'cd', project_directory, '&&',
+        f'{venv}\\Scripts\\activate.bat', '&&',
+        'pip', 'install', '-r', f'{requirement_file}'
+        ], shell = True)
+
 
 def activate_venv(project_directory, venv):
     return os.system(f'cd "{project_directory}"\n' + venv + '\\Scripts\\activate.bat\n')
@@ -35,4 +43,4 @@ def install_system_library(library):
     subprocess.run(['pip', 'install', library], shell = True)
 
 def check_venv(project_directory, venv):
-    return os.path.exists(f'{project_directory}/{venv}/bin/activate')
+    return os.path.exists(f'{project_directory}\\{venv}\\Scripts\\activate.bat')
