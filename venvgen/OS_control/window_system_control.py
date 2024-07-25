2e5638e059e26f2a6548422684b52c688dcc3229
tuanisworkingonsomeprojects <127831837+tuanisworkingonsomeprojects@users.noreply.github.com>
2024-07-26 01:16:53 +0800

    Update window_system_control.py

--- venvgen/OS_control/window_system_control.py
+++ venvgen/OS_control/window_system_control.py
@@ -22,25 +22,38 @@ def create_venv(project_directory, project_name):
 
 def install_libraries(project_directory, venv, libraries):
     # os.system(f'cd "{project_directory}"\n' + venv + '\\Scripts\\activate.bat\npip install ' + libraries)
+
     subprocess.run([
-        'cd', project_directory, '&&', 
-        f'{venv}\\Scripts\\activate.bat', '&&', 
-        'pip', 'install'
-        ] + libraries, shell = True)
+        f'{project_directory}\\{venv}\\Scripts\\pip', 'install', libraries
+        ], shell = True)
     
 def install_libraries_with_requirement(project_directory, venv, requirement_file):
     subprocess.run([
-        'cd', project_directory, '&&',
-        f'{venv}\\Scripts\\activate.bat', '&&',
-        'pip', 'install', '-r', f'{requirement_file}'
+        f'{project_directory}\\{venv}\\Scripts\\pip', 'install', '-r', f'{requirement_file}'
         ], shell = True)
 
 
 def activate_venv(project_directory, venv):
-    return os.system(f'cd "{project_directory}"\n' + venv + '\\Scripts\\activate.bat\n')
+    return subprocess.run([
+        'cd',
+        f'{project_directory}\\{venv}\\Scripts', '&&',
+        'activate.bat'
+    ], capture_output = True, text = True, shell = True).returncode
+
 
 def install_system_library(library):
     subprocess.run(['pip', 'install', library], shell = True)
 
 def check_venv(project_directory, venv):
     return os.path.exists(f'{project_directory}\\{venv}\\Scripts\\activate.bat')
+
+def get_requirement(project_dir, venv):
+    return subprocess.run([
+        f'{project_dir}\\{venv}\\Scripts\\pip', 'freeze'
+    ], shell = True, capture_output = True, text = True).stdout
+
+def uninstall_libraries(project_directory, venv, libraries):
+    subprocess.run([
+        f'{project_directory}\\{venv}\\Scripts\\pip', 'uninstall', libraries, '-y'
+    ], shell = True)
+
